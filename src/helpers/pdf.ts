4a6d12789214ddd1407fffffdfb7fe891a002b4f
Wonjang Son <sondaniel.88@gmail.com>
2022-06-10 10:07:22 +0200

    Added try/catch

--- src/helpers/pdf.ts
+++ src/helpers/pdf.ts
@@ -13,23 +13,28 @@ type HtmlData = {
 };
 
 export const generateOffPdf = async (filename: string, htmlData: HtmlData) => {
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-  });
+  try {
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
 
-  const page = await browser.newPage();
+    const page = await browser.newPage();
 
-  const offHtmlPath = path.resolve(__dirname, `../../html/day_off.html`);
-  const document = addVarsToHtml(offHtmlPath, htmlData);
+    const offHtmlPath = path.resolve(__dirname, `../../html/day_off.html`);
+    const document = addVarsToHtml(offHtmlPath, htmlData);
 
-  await page.setContent(document, { waitUntil: "networkidle2" });
+    await page.setContent(document, { waitUntil: "networkidle2" });
 
-  const pdfBuffer = await page.pdf({
-    format: "A4",
-  });
+    const pdfBuffer = await page.pdf({
+      format: "A4",
+    });
 
-  await browser.close();
+    await browser.close();
 
-  return { filename, content: pdfBuffer };
+    return { filename, content: pdfBuffer };
+  } catch (error) {
+    if (error instanceof Error) throw error;
+    throw new Error(error);
+  }
 };
